6c2e0f86978de7168492a7e1de30d3db68f808e0
Unknown <fede-macias@hotmail.com>
2017-05-21 14:01:28 -0300

    Tp post correccion
    Faltaria revisar el resto de las funciones de compatibilidad para hacer bien las consultas

--- Tp logico.pl
+++ Tp logico.pl
@@ -1,40 +1,37 @@
-departamento(tinsmithcircle1774, 700).
-departamento(avmoreno708,2000).
-departamento(avsiempreviva742, 1000).
-departamento(callefalsa123, 200).
-jardin(tinsmithcircle1774).
-jardin(avmoreno708).
-jardin(avsiempreviva742).
-cantidadAmbientes(tinsmithcircle1774,3).
-cantidadAmbientes(avmoreno708,7).
-cantidadAmbientes(avsiempreviva742,4).
-cantidadAmbientes(tinsmithcircle1774,3).
-pileta(avmoreno708,30).
+precioDepto(tinsmithCircle1774, 700).
+precioDepto(avMoreno708,2000).
+precioDepto(avSiempreviva742, 1000).
+precioDepto(calleFalsa123, 200).
+tieneCaracteristicas(tinsmithCircle1774,ambientes(3)).
+tieneCaracteristicas(tinsmithCircle1774,jardin).
+tieneCaracteristicas(avMoreno708,ambientes(7)).
+tieneCaracteristicas(avMoreno708,jardin).
+tieneCaracteristicas(avMoreno708,pileta(30)).
+tieneCaracteristicas(avSiempreviva742,ambientes(4)).
+tieneCaracteristicas(avSiempreviva742,jardin).
+tieneCaracteristicas(calleFalsa123,ambientes(3)).
 
-mismaCantidadAmbientes(Propiedad1,Propiedad2):-
-  cantidadAmbientes(Propiedad1,Metros),
-  cantidadAmbientes(Propiedad2,Metros).
+persona(carlos).
+persona(ana).
+persona(maria).
+persona(pedro).
+persona(chamaleon).
 
-%--version 1--
-quiereCantidadDeAmbientes(carlos,3).
-quiereCantidadDeAmbientes(maria,2).
-quiereCantidadDeAmbientes(pedro,2).
-quiereCantidadDeAmbientes(chameleon, 2).
-
-quiereJardin(carlos).
-quiereJardin(chameleon).
-
-quierePiscina(ana,100).
-quierePiscina(carlos,30).
-quierePiscina(maria,15).
-quierePiscina(pedro,15).
-quierePiscina(chameleon,100).
+caracteristicasDeseadas(carlos,ambientes(3)).
+caracteristicasDeseadas(carlos,jardin).
+caracteristicasDeseadas(ana,pileta(100)).
+caracteristicasDeseadas(maria,ambientes(2)).
+caracteristicasDeseadas(maria,pileta(15)).
+caracteristicasDeseadas(pedro,Caracteristica):-
+  caracteristicasDeseadas(maria,Caracteristica)
+caracteristicasDeseadas(chamaleon,Caracteristica):-
+  Persona /= chamaleon,
+  caracteristicasDeseadas(Persona,Caracteristica).
 
 propiedadCompatible(Nombre, Propiedad):-
   compatibleJardin(Nombre, Propiedad),
   compatibleCantidadAmbientes(Nombre,Propiedad).
   compatiblePileta(Nombre,Propiedad).
-
 
 compatibleJardin(Nombre,Propiedad):-
   quiereJardin(Nombre),
@@ -50,28 +47,7 @@ compatiblePileta(Nombre,Propiedad):-
   pileta(Propiedad, TamanioQueTiene),
   TamanioQueTiene>=TamanioQueQuiere.
 
- %usuario(carlos,(cantidadAmbientes(depto,3),jardin(depto),noPileta)).
+/** Consultas:
 
 
-
- % Consultas
-
- /** 1) 1 ?- pileta(Cual,30).
-    Cual = avmoreno708.
- */
-
- /** 2) 2 ?- mismaCantidadAmbientes(X,Y).
- X = Y, Y = tinsmithcircle1774 ;
- X = tinsmithcircle1774,
- Y = callefalsa123 ;
- X = Y, Y = avmoreno708 ;
- X = Y, Y = avsiempreviva742 ;
- X = callefalsa123,
- Y = tinsmithcircle1774 ;
- X = Y, Y = callefalsa123.
- */
-
-/**
- 4) ?- cantidadAmbientes(Cual,2).
-false.
 */

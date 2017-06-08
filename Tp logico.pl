49c73d3cf8103b47fece54a3d95d3da9b957163d
Mailen Verdun <verdunmailen@gmail.com>
2017-06-08 17:11:06 -0300

    Arreglo de Bug y Consultas
    Arregle el bug q no encontrabamos menos el de efectividad. Hice las consultas posibles con el codigo que funciona bien.

--- Tp logico.pl
+++ Tp logico.pl
@@ -2,27 +2,24 @@ precioDepto(tinsmithCircle1774, 700).
 precioDepto(avMoreno708,2000).
 precioDepto(avSiempreviva742, 1000).
 precioDepto(calleFalsa123, 200).
+
 tieneCaracteristicas(tinsmithCircle1774,ambientes(3)).
 tieneCaracteristicas(tinsmithCircle1774,jardin).
-
-
 tieneCaracteristicas(tinsmithCircle1774,aireAcondicionado).
 tieneCaracteristicas(tinsmithCircle1774,extractor).
 tieneCaracteristicas(tinsmithCircle1774,calefaccionAGas).
+
 tieneCaracteristicas(avMoreno708,ambientes(7)).
 tieneCaracteristicas(avMoreno708,jardin).
 tieneCaracteristicas(avMoreno708,pileta(30)).
-
-
 tieneCaracteristicas(avMoreno708,aireAcondicionado).
 tieneCaracteristicas(avMoreno708,extractor).
 tieneCaracteristicas(avMoreno708,lozaRadiante).
 tieneCaracteristicas(avMoreno708,vidriosDobles).
+
 tieneCaracteristicas(avSiempreviva742,ambientes(4)).
 tieneCaracteristicas(avSiempreviva742,jardin).
-
 tieneCaracteristicas(avSiempreviva742,calefaccionAGas).
-%15
 tieneCaracteristicas(calleFalsa123,ambientes(3)).
 
 persona(carlos).
@@ -33,10 +30,11 @@ persona(chamaleon).
 
 caracteristicasDeseadas(carlos,ambientes(3)).
 caracteristicasDeseadas(carlos,jardin).
+
 caracteristicasDeseadas(ana,pileta(100)).
 caracteristicasDeseadas(ana, aireAcondicionado).
 caracteristicasDeseadas(ana, vidriosDobles).
-%30
+
 caracteristicasDeseadas(maria,ambientes(2)).
 caracteristicasDeseadas(maria,pileta(15)).
 
@@ -44,17 +42,24 @@ caracteristicasDeseadas(pedro,Caracteristica):-
   caracteristicasDeseadas(maria,Caracteristica).
 caracteristicasDeseadas(pedro,lozaRadiante).
 caracteristicasDeseadas(pedro,vidriosDobles).
+
 caracteristicasDeseadas(chamaleon,Caracteristica):-
 	persona(Persona),
   Persona \= chamaleon,
   caracteristicasDeseadas(Persona,Caracteristica).
 
 
-cumpleTodo(Nombre,Depto):-
-	
-	forall(persona(Nombre),(caracteristicasDeseadas(Nombre, Caracteristica),tieneCaracteristicas(Depto, Caracteristica))).
 
-mejorOpcion(Nombre, Depto):-
+
+
+cumpleTodo(Nombre,Depto):- RECURSIVIDAD??
+persona(Nombre),
+	forall(caracteristicasDeseadas(Nombre, Caracteristica),tieneCaracteristicas(Depto, Caracteristica)).
+
+
+
+mejorOpcion(Nombre, Depto):-DEBE VER TODO EL SUBCONJUNTO Y ELEGIR EL MENOR(EJEMPLO MAGOS)
+  persona(Nombre),
 	cumpleTodo(Nombre, Depto),
 	cumpleTodo(Nombre, Depto2),
 	precioDepto(Depto, Precio),
@@ -62,20 +67,22 @@ mejorOpcion(Nombre, Depto):-
 	Depto\=Depto2,
 	Precio=<Precio2.
 
-efectividad(Cantidad):-
 
+/*
+efectividad(Cantidad):-
 	findall(Persona,(persona(Persona),cumpleTodo(Persona, _ ), PersonasSatisfechas),
 	list_to_set(PersonasSatisfechas,PersonasSatisfechasSinRepetir),
 	findall(Persona,(persona(Persona),PersonasEnElMundo),
 	length(PersonasSatisfechasSinRepetir,Cantidad1),
 	length(PersonasEnElMundo, Cantidad2),
-	Cantidad is Cantidad1 \ Cantidad2.
+	Cantidad is (Cantidad1 / Cantidad2).
+*/
 
-esPropiedadTop(Propiedad):-
-	tieneCaractersiticas(Propiedad,aireAcondicionado),
+
+
+esPropiedadTop(Propiedad):-%BIEN!
+	tieneCaracteristicas(Propiedad,aireAcondicionado),
 	not(esChica(Propiedad)).
-	
-
 
 esChica(Propiedad):-
 	tieneCaracteristicas(Propiedad,ambientes(CantidadDeAmbientes)),
@@ -86,6 +93,54 @@ esChica(Propiedad):-
 
 tieneAmbientes(Propiedad):-tieneCaracteristicas(Propiedad,ambientes(_)).
 /** Consultas:
+1. Si existe alguna propiedad con una piscina de 30 metros, y si es que existe, cuál es.
+?-tieneCaracteristicas(Propiedad,pileta(30)).
+Propiedad = avMoreno708.
 
 
+2. Si existe alguna propiedad que tenga la misma cantidad de ambientes que otra, y si existe, cuál/es son solución.
+?- tieneCaracteristicas(Propiedad,ambientes(CantAmbientes)),tieneCaracteristicas(Propiedad2,ambientes(CantAmbientes)),Propiedad\=Propiedad2.
+Propiedad = tinsmithCircle1774,
+CantAmbientes = 3,
+Propiedad2 = calleFalsa123 ;
+Propiedad = calleFalsa123,
+CantAmbientes = 3,
+Propiedad2 = tinsmithCircle1774 ;
+
+
+3. Mostrar qué quiere Pedro. Por ejemplo, una posible solución a lo que quiere, es una piscina de 15 m³.
+?- caracteristicasDeseadas(pedro,Caracteristica).
+Caracteristica = ambientes(2) ;
+Caracteristica = pileta(15) ;
+Caracteristica = lozaRadiante ;
+Caracteristica = vidriosDobles.
+
+
+4. Si existe alguna propiedad que cumpla con tener al menos 2 ambientes.
+ ?- tieneCaracteristicas(Propiedad,ambientes(2)).
+ false.
+
+5. Cuál propiedad tiene (cumple) algo de lo que quiere Pedro.
+
+
+6. Qué se está deseando de la propiedad de Av. Moreno 708 (y que ésta lo cumpla).
+
+
+7. Determinar qué característica fue solicitada y ninguna propiedad la pudo cumplir.
+
+
+8. Qué persona se relaciona con cuál propiedad mediante ese predicado.
+
+
+9. Qué persona se relaciona con cuál propiedad (asegurarse de que ambas alternativas encuentren las mismas
+soluciones, ya sean en el mismo orden o no).
+
+
+10. Qué efectividad tiene nuestro sistema.
+
+
+11. Determinar cuáles son las propiedades top.
+?- esPropiedadTop(PropiedadTop).
+PropiedadTop = tinsmithCircle1774 ;
+PropiedadTop = avMoreno708.
 */

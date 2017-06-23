0b2918767fe887e2fb4de019d1237f6f63a32a40
Mailen Verdun <verdunmailen@gmail.com>
2017-06-22 22:58:41 -0300

    Correcciones / Cosas que hacer
    Arregle los errores de compilacion y varios predicados que no funcionaban por elcambio de nombre de los predicacos anteriores. Arregle las consultas.
    
    A hacer:
    •Las consultas no hechas es porq hay problemas con los predicados.Ej: estaSatisfecho es 0 la lista, por lo que efectividad tira error aritmetico.
    •Hay que hacer un separador de listas para las instalaciones.
    •Predicados y consulta con not.

--- Tp logico.pl
+++ Tp logico.pl
@@ -5,15 +5,15 @@ precioDepto(calleFalsa123, 200).
 
 tieneCaracteristicas(tinsmithCircle1774,ambientes(3)).
 tieneCaracteristicas(tinsmithCircle1774,jardin).
-tieneCaracteristicas(tinsmithCircle1774,instalaciones([aireAcondicionado,extractor,calefaccionAGas]).
+tieneCaracteristicas(tinsmithCircle1774,instalaciones([aireAcondicionado,extractor,calefaccionAGas])).
 tieneCaracteristicas(avMoreno708,ambientes(7)).
 tieneCaracteristicas(avMoreno708,jardin).
 tieneCaracteristicas(avMoreno708,pileta(30)).
-tieneCaracteristicas(avMoreno708,instalaciones([vidriosDobles,lozaRadiante,extractor,aireAcondicionado]).
+tieneCaracteristicas(avMoreno708,instalaciones([vidriosDobles,lozaRadiante,extractor,aireAcondicionado])).
 tieneCaracteristicas(avSiempreviva742,ambientes(4)).
 tieneCaracteristicas(avSiempreviva742,jardin).
 tieneCaracteristicas(avSiempreviva742,calefaccionAGas).
-tieneCaracteristicas(avSiempreviva742,instalaciones([calefaccionAGas]).
+tieneCaracteristicas(avSiempreviva742,instalaciones([calefaccionAGas])).
 tieneCaracteristicas(calleFalsa123,ambientes(3)).
 
 persona(carlos).
@@ -36,23 +36,23 @@ caracteristicaDeseada(pedro,vidriosDobles).
 caracteristicaDeseada(chamaleon,Caracteristica):-
 	persona(Persona),
   Persona \= chamaleon,
-  caracteristicasDeseadas(Persona,Caracteristica).
+  caracteristicaDeseada(Persona,Caracteristica).
 
 cumpleCaracteristica(Propiedad,Caracteristica):-
-  caracteristicasDeseadas(Caracteristica),
+  caracteristicaDeseada(_,Caracteristica),
   tieneCaracteristicas(Propiedad,Caracteristica).
 
 cumpleCaracteristica(Propiedad,Caracteristica):-
-  caracteristicasDeseadas(Caracteristica),
+  caracteristicaDeseada(_,Caracteristica),
   analisisCaracteristica(Caracteristica,Propiedad).
 
 analisisCaracteristica(pileta(Metros),Propiedad):-
   tieneCaracteristicas(Propiedad,pileta(OtrosMetros)),
-  Metros<OtrosMetros.
+  Metros=<OtrosMetros.
 
 analisisCaracteristica(ambientes(Metros),Propiedad):-
     tieneCaracteristicas(Propiedad,ambientes(OtrosMetros)),
-    Metros<OtrosMetros.
+    Metros=<OtrosMetros.
 
 analisisCaracteristica(instalaciones(Instalacion),Propiedad):-
   tieneCaracteristicas(Propiedad,instalaciones(Instalacion)).
@@ -60,7 +60,7 @@ analisisCaracteristica(instalaciones(Instalacion),Propiedad):-
 cumpleTodo(Nombre,Depto):-
 persona(Nombre),
 precioDepto(Depto,_),
-	forall(caracteristicasDeseadas(Nombre, Caracteristica),cumpleCaracteristica(Depto, Caracteristica)).
+	forall(caracteristicaDeseada(Nombre, Caracteristica),cumpleCaracteristica(Depto, Caracteristica)).
 
 mejorOpcion(Nombre, Depto):-
   cumpleTodo(Nombre,Depto),
@@ -69,15 +69,11 @@ mejorOpcion(Nombre, Depto):-
   %Hacer opcion 2 con el doble NOT
 
 efectividad(Cantidad):-
-  todasLasSatisfechas(Persona),
-  todasLasPersonas(Persona),
+  todasLasSatisfechas(Personas),
+  todasLasPersonas(Personas),
 	length(PersonasSatisfechasSinRepetir,Cantidad1),
 	length(PersonasEnElMundo, Cantidad2),
 	Cantidad is Cantidad1 / Cantidad2.
-
-estaSatisfecho(Persona):-
-  persona(Persona),
-  cumpleTodo(Persona, _ ).
 
 todasLasPersonas(Persona):-
   findall(Persona,persona(Persona),PersonasEnElMundo).
@@ -86,12 +82,16 @@ todasLasSatisfechas(Persona):-
 findall(Persona,(estaSatisfecho(Persona)), PersonasSatisfechas),
 list_to_set(PersonasSatisfechas,PersonasSatisfechasSinRepetir).
 
+estaSatisfecho(Persona):-
+  persona(Persona),
+  cumpleTodo(Persona, _ ).
+
 esPropiedadTop(Propiedad):-
-	tieneCaracteristicas(Propiedad,instalaciones(aireAcondicionado)),
+	cumpleCaracteristica(Propiedad,instalaciones([aireAcondicionado])),
 	not(esChica(Propiedad)).
 
 esChica(Propiedad):-
-	tieneCaracteristicas(Propiedad,ambientes(1)),
+	tieneCaracteristicas(Propiedad,ambientes(1)).
 
 esChica(Propiedad):-
   precioDepto(Propiedad,_).
@@ -119,46 +119,146 @@ CantAmbientes = 3,
 Propiedad2 = tinsmithCircle1774 ;
 
 3. Mostrar qué quiere Pedro. Por ejemplo, una posible solución a lo que quiere, es una piscina de 15 m³.
-?- caracteristicasDeseadas(pedro,Caracteristica).
+?- caracteristicaDeseada(pedro,Caracteristica).
 Caracteristica = ambientes(2) ;
 Caracteristica = pileta(15) ;
 Caracteristica = lozaRadiante ;
 Caracteristica = vidriosDobles.
 
 4. Si existe alguna propiedad que cumpla con tener al menos 2 ambientes.
- ?- tieneCaracteristicas(Propiedad,ambientes(2)).
- false.
-
-5. Cuál propiedad tiene (cumple) algo de lo que quiere Pedro.
-?- cumpleCaracteristica(pedro,Propiedad).
+?- cumpleCaracteristica(Propiedad,ambientes(2)).
+Propiedad = tinsmithCircle1774 ;
 Propiedad = avMoreno708 ;
+Propiedad = avSiempreviva742 ;
+Propiedad = calleFalsa123 ;
+Propiedad = tinsmithCircle1774 ;
+Propiedad = avMoreno708 ;
+Propiedad = avSiempreviva742 ;
+Propiedad = calleFalsa123 ;
+Propiedad = tinsmithCircle1774 ;
+Propiedad = avMoreno708 ;
+Propiedad = avSiempreviva742 ;
+Propiedad = calleFalsa123 ;
 Propiedad = tinsmithCircle1774 ;
 Propiedad = avMoreno708 ;
 Propiedad = avSiempreviva742 ;
 Propiedad = calleFalsa123 ;
 
+5. Cuál propiedad tiene (cumple) algo de lo que quiere Pedro.
+caracteristicaDeseada(pedro,Caracteristica),cumpleCaracteristica(Propiedad,Caracteristica).
+Caracteristica = ambientes(2),
+Propiedad = tinsmithCircle1774 ;
+Caracteristica = ambientes(2),
+Propiedad = avMoreno708 ;
+Caracteristica = ambientes(2),
+Propiedad = avSiempreviva742 ;
+Caracteristica = ambientes(2),
+Propiedad = calleFalsa123 ;
+Caracteristica = ambientes(2),
+Propiedad = tinsmithCircle1774 ;
+Caracteristica = ambientes(2),
+Propiedad = avMoreno708 ;
+Caracteristica = ambientes(2),
+Propiedad = avSiempreviva742 ;
+Caracteristica = ambientes(2),
+Propiedad = calleFalsa123 ;
+Caracteristica = ambientes(2),
+Propiedad = tinsmithCircle1774 ;
+Caracteristica = ambientes(2),
+Propiedad = avMoreno708 ;
+Caracteristica = ambientes(2),
+Propiedad = avSiempreviva742 ;
+Caracteristica = ambientes(2),
+Propiedad = calleFalsa123 ;
+Caracteristica = ambientes(2),
+Propiedad = tinsmithCircle1774 ;
+Caracteristica = ambientes(2),
+Propiedad = avMoreno708 ;
+Caracteristica = ambientes(2),
+Propiedad = avSiempreviva742 ;
+Caracteristica = ambientes(2),
+Propiedad = calleFalsa123 ;
+Caracteristica = pileta(15),
+Propiedad = avMoreno708 ;
+Caracteristica = pileta(15),
+Propiedad = avMoreno708 ;
+Caracteristica = pileta(15),
+Propiedad = avMoreno708 ;
+Caracteristica = pileta(15),
+Propiedad = avMoreno708 ;
+false.
+
 6. Qué se está deseando de la propiedad de Av. Moreno 708 (y que ésta lo cumpla).
+caracteristicaDeseada(_,CaracteristicaQueCumpleMinimamente),cumpleCaracteristica(avMoreno708,CaracteristicaQueCumpleMinimamente).
+CaracteristicaQueCumpleMinimamente = ambientes(3) ;
+CaracteristicaQueCumpleMinimamente = ambientes(3) ;
+CaracteristicaQueCumpleMinimamente = jardin ;
+CaracteristicaQueCumpleMinimamente = jardin ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = ambientes(3) ;
+CaracteristicaQueCumpleMinimamente = ambientes(3) ;
+CaracteristicaQueCumpleMinimamente = jardin ;
+CaracteristicaQueCumpleMinimamente = jardin ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = ambientes(2) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+CaracteristicaQueCumpleMinimamente = pileta(15) ;
+false.
 
 7. Determinar qué característica fue solicitada y ninguna propiedad la pudo cumplir.
 
 8. Qué persona se relaciona con cuál propiedad mediante ese predicado.
 ?- cumpleTodo(Nombre,Propiedad).
+cumpleTodo(Nombre,Propiedad).
 Nombre = carlos,
 Propiedad = tinsmithCircle1774 ;
+Nombre = carlos,
+Propiedad = avMoreno708 ;
+Nombre = carlos,
+Propiedad = avSiempreviva742 ;
+Nombre = maria,
+Propiedad = avMoreno708 ;
 false.
 
 9. Qué persona se relaciona con cuál propiedad (asegurarse de que ambas alternativas encuentren las mismas
 soluciones, ya sean en el mismo orden o no).
-?- mejorOpcion(Persona,Propiedad).
+?-  mejorOpcion(Persona,Propiedad).
 Persona = carlos,
 Propiedad = tinsmithCircle1774 ;
+Persona = maria,
+Propiedad = avMoreno708 ;
 
 10. Qué efectividad tiene nuestro sistema.
-?- efectividad(A).
-A = 0.2.
+
 
 11. Determinar cuáles son las propiedades top.
 ?- esPropiedadTop(PropiedadTop).
-PropiedadTop = tinsmithCircle1774 ;
-PropiedadTop = avMoreno708.
+
 */
